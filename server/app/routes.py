4fd6e8b9b61ee5c88f950aaca321c2f40e214cce
Temitope <tope_olajide@outlook.com>
2019-11-17 12:06:04 +0100

    Add more routes

--- server/app/routes.py
+++ server/app/routes.py
@@ -40,6 +40,71 @@ def index():
 def json_example():
     text = request.form.get('text')
     return text
+@app.route('/lsa-summarization', methods=['POST'])
+def summarize_document():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    numberOfSentence = req_data['numberOfSentence']
+    result = []
+    parser = PlaintextParser.from_string(textDocument,Tokenizer("english"))
+    summarizer_lsa = LsaSummarizer()
+    summary = summarizer_lsa(parser.document, numberOfSentence)
+    for sentence in summary:
+        print (sentence)
+        result.append(str(sentence))
+        print (result)
+    return json.dumps(result)
 
+@app.route('/tokenize-document', methods=['POST'])
+def tokenize_document():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    wordTokens = tokenizeDocument(textDocument)
+    return json.dumps(wordTokens)
+
+@app.route('/remove-stopword', methods=['POST'])
+def remove_stopword():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    filtered_sentence = removeStopword(textDocument) 
+    return json.dumps(filtered_sentence)
+
+@app.route('/stem-document', methods=['POST'])
+def stem_document():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    stemmed_word = stemDocument(textDocument)
+    return json.dumps(stemmed_word)
+
+@app.route('/tf-idf', methods =['POST'])
+def svd():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    result = {}
+    text = stemDocument(textDocument)
+    #TfidfVectorizer().fit_transform allows only raw text document or string as parameter
+    textToString = ' '.join(text)
+    tfidf = TfidfVectorizer()
+    response = tfidf.fit_transform([textToString])
+    feature_names = tfidf.get_feature_names()
+    for col in response.nonzero()[1]:
+        result [feature_names[col]] = response[0, col]
+    resultToJson = json.dumps(result)
+    return resultToJson
+@app.route('/generate-svd', methods =['POST'])
+def svd2():
+    req_data = request.get_json()
+    textDocument = req_data['textDocument']
+    text = stemDocument(textDocument)
+    count_vectorizer = CountVectorizer()
+    data = count_vectorizer.fit_transform(text).toarray()
+    count_vectorizer._validate_vocabulary()
+    featurenames = count_vectorizer.get_feature_names()
+    tfidf = TfidfTransformer()
+    tfidfMatrix = tfidf.fit_transform(data) 
+    svd = TruncatedSVD(n_components = 30)
+    svdMatrix = svd.fit_transform(tfidfMatrix)
+    print (svdMatrix)
+    return json.dumps(svdMatrix.tolist())
 
 
